72a7a3bbbca85069f471b76a98a3540ca835b72d
Mark Ramy <121722469+markramyy@users.noreply.github.com>
2024-12-17 01:22:50 +0200

    Create transaction model and widget

--- lib/screens/widgets/transaction.dart
+++ lib/screens/widgets/transaction.dart
@@ -1,1 +1,15 @@
+class Transaction {
+    final String userId;
+    final DateTime orderDate;
+    final List<Map<String, dynamic>> products;
+    final double totalAmount;
+    final String status;
 
+    Transaction({
+        required this.userId,
+        required this.orderDate,
+        required this.products,
+        required this.totalAmount,
+        required this.status,
+    });
+}
